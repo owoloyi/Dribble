f244d4c0ec0f6272035843127021d20522a59c12
owoloyi <111381620+owoloyi@users.noreply.github.com>
2024-09-23 15:46:05 +0100

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero(){
             Over 3 million ready-to-work creatives!
             </div>
             <div className="h1s">The world’s destination😂😂😂😂😂 <br /> for design</div>
-            <div className="subTitle">Get inspired by the work of millions of top-rated designers & agencies around the world.</div>
+            <div className="subTitle">Get inspired by the work of million of top-rated designers & agencies around the world.</div>
             <div className="get-started">Get started</div>
         </div>
     )
